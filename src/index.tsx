8171f049df1386eb4df51725c1ea36ca3ab9bea9
guokaigdg <guokaigdg@gmail.com>
2023-06-08 19:38:27 +0800

    feat: ✨ add HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom';
-import {basename} from '@/utils/variable';
+// import {BrowserRouter} from 'react-router-dom';
+// import {basename} from '@/utils/variable';
+import {HashRouter} from 'react-router-dom';
 import {StoresProvider, stores} from '@/store';
 import '@/assets/icons/index';
 import App from './App';
@@ -10,9 +11,14 @@ const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement as HTMLDivElement);
 
 root.render(
-    <BrowserRouter basename={basename}>
+    // <BrowserRouter basename={basename}>
+    //     <StoresProvider value={stores}>
+    //         <App />
+    //     </StoresProvider>
+    // </BrowserRouter>
+    <HashRouter>
         <StoresProvider value={stores}>
             <App />
         </StoresProvider>
-    </BrowserRouter>
+    </HashRouter>
 );
