cd5c9458c44828d3294382806ba231f85579e5e6
guokaigdg <guokaigdg@gmail.com>
2023-08-24 11:15:52 +0800

    feat: 更新package.json

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     plugins: {
         'postcss-mobile-forever': {
-            rootSelector: "#root .app", // 根元素，用于设置居中
+            rootSelector: '#root .app', // 根元素，用于设置居中
             viewportWidth: 750, // 窗口宽度: UI设计稿给的手机的窗口宽度 （xx/750*100vw）
             maxDisplayWidth: 560, // 最大宽度
             disableLandscape: true, // 禁止生成横屏媒体查询
@@ -11,7 +11,7 @@ module.exports = {
             mobileUnit: 'vw', // 指定需要转换成的视窗单位，默认vw
             selectorBlackList: [], // 指定不转换为视窗单位的类名，
             valueBlackList: ['1px solid'], // 指定不用转换的值
-            exclude: [/node_modules/], // 设置忽略文件，用正则做目录名匹配
+            exclude: [/node_modules/] // 设置忽略文件，用正则做目录名匹配
         }
     }
 };
